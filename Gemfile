f1454e930707c6c4229f38de0c01a54a8283ec7d
Yusuke KON <yusukedrive@gmail.com>
2015-05-31 17:42:48 +0900

    upload file

--- Gemfile
+++ Gemfile
@@ -38,3 +38,4 @@ end
 
 gem "neat"
 gem "bitters"
+gem "carrierwave"
--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -72,6 +72,6 @@ class ImagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def image_params
-      params.require(:image).permit(:img_path)
+      params.require(:image).permit(:img_path, :image, :image_cache, :remove_image)
     end
 end
